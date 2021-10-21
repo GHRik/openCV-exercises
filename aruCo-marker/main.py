a510cf35102471d45c0f879179ea0f372f3a0015
Damian Panasiuk <damian.panasiuk@tieto.com>
2021-10-21 15:28:04 +0200

    Uncomment videograbber section
    Now script properly get frame from videograbber.
    
    Implements: Exercise-3

--- aruCo-marker/main.py
+++ aruCo-marker/main.py
@@ -2,24 +2,23 @@ import cv2
 
 if __name__ == '__main__':
 
-    #cap = cv2.VideoCapture(0)
-    #if not (cap.isOpened()):
-    #    print("Could not open video device")
-    #    exit()
-    #print("Camera found... Start configuration")
-    #cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    #cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-    #print("Configuration done...")
+    cap = cv2.VideoCapture(0)
+    if not (cap.isOpened()):
+        print("Could not open video device")
+        exit()
+    print("Camera found... Start configuration")
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    print("Configuration done...")
     cv2.startWindowThread()
     cv2.namedWindow("preview")
 
-    while (True):
-        #ret, frame = cap.read()
-        frame = cv2.imread("images/arucoMarker.jpg")
+    while True:
+        ret, frame = cap.read()
         arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_50)
         arucoParams = cv2.aruco.DetectorParameters_create()
         (corners, ids, rejected) = cv2.aruco.detectMarkers(frame, arucoDict, parameters=arucoParams)
-        if len(corners) > 0 :
+        if len(corners) > 0:
             for i in range(len(ids)):
                 realCorners = corners[i].reshape((4, 2))
                 (topLeft, topRight, bottomRight, bottomLeft) = realCorners
@@ -37,8 +36,9 @@ if __name__ == '__main__':
                 cY = int((topLeft[1] + bottomRight[1]) / 2.0)
                 frame = cv2.circle(frame, (cX, cY), 4, (0, 0, 255), -1)
 
-                frame = cv2.putText(frame, str(ids[i]), (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                frame = cv2.putText(frame, str(ids[i]), (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                                    (0, 255, 0), 2)
 
-        cv2.imshow('preview',frame)
+        cv2.imshow('preview', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
